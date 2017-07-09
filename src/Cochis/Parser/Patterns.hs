b69e68a1eb2071a2fe892f6e2f158215a0648627
lyxia <lysxia@gmail.com>
2017-07-09 08:46:48 -0400

    Missing parentheses in TypeApp

--- src/Cochis/Parser/Patterns.hs
+++ src/Cochis/Parser/Patterns.hs
@@ -23,7 +23,7 @@ unkinded = traverse unkinded'
     unkinded' _ = Nothing
     
 pattern HsTApp :: Exp -> Type -> Exp
-pattern HsTApp e t = App e (TypeApp t)
+pattern HsTApp e t = App e (TypeApp' t)
 
 pattern HsIQuery :: Type -> Exp
 pattern HsIQuery t = ExpTypeSig ExprHole t
@@ -32,7 +32,7 @@ pattern HsIApp :: Exp -> Exp -> Exp
 pattern HsIApp e0 e1 = App (App (LitVar "with") e1) e0
 
 pattern HsIAbs :: Type -> Exp -> Exp
-pattern HsIAbs t e = App (App (LitVar "implicit") (TypeApp t)) e
+pattern HsIAbs t e = App (App (LitVar "implicit") (TypeApp' t)) e
 
 pattern HsTyIFun :: Type -> Type -> Type
 pattern HsTyIFun t0 t1 = TyInfix t0 (LitSym "~>") t1
@@ -48,3 +48,11 @@ pattern LitVar s = HsVar (Ident s)
 
 pattern LitSym :: String -> QName
 pattern LitSym s = UnQual (Symbol s)
+
+-- With automatic parentheses as a constructor.
+pattern TypeApp' :: Type -> Exp
+pattern TypeApp' t <- TypeApp t
+  where TypeApp' t = TypeApp (case t of
+          TyVar _ -> t
+          TyCon _ -> t
+          _ -> TyParen t)
